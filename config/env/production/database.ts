7cd5e109635f51e78080e992bae5f44d7fa33534
nikodem bilczewski <bilczewski.nikodem@gmail.com>
2023-07-14 18:14:28 +0200

    change database.ts

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -2,11 +2,14 @@ export default ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
+      host: env(
+        "DATABASE_HOST",
+        "db-strapi.cxcmwaj0diye.eu-north-1.rds.amazonaws.com"
+      ),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "database_strapi"),
-      user: env("DATABASE_USERNAME", ""),
-      password: env("DATABASE_PASSWORD", ""),
+      database: env("DATABASE_NAME", "db-strapi"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "3p7-gQSA?w-"),
     },
     useNullAsDefault: true,
   },
