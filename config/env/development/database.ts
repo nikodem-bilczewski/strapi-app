3b483885e11cc7df463c419bcc0a3892160e49a7
nikodem bilczewski <bilczewski.nikodem@gmail.com>
2023-07-16 18:50:33 +0200

    change config

--- config/env/development/database.ts
+++ config/env/development/database.ts
@@ -7,7 +7,7 @@ export default ({ env }) => ({
         "strapi-app-database.cxcmwaj0diye.eu-north-1.rds.amazonaws.com"
       ),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi-app-database"),
+      database: env("DATABASE_NAME", "postgres"),
       user: env("DATABASE_USERNAME", "postgres"),
       password: env("DATABASE_PASSWORD", "3p7-gQSA?w-"),
     },
