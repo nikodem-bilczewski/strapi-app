11b1496a18e1e72ed6f32c46e953e1b68e8503ce
nikodem bilczewski <bilczewski.nikodem@gmail.com>
2023-07-16 20:39:07 +0200

    update plugins.ts and middleware.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -7,7 +7,8 @@ export default ({ env }) => ({
         secretAccessKey: env("AWS_ACCESS_SECRET"),
         region: env("AWS_REGION"),
         params: {
-          Bucket: env("AWS_BUCKET_NAME"),
+          ACL: env("AWS_ACL", "public-read"),
+          Bucket: env("AWS_BUCKET"),
         },
       },
     },
