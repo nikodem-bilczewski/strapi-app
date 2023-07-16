7444270e8939bc4dbccb52b0445e98b5273657df
nikodem bilczewski <bilczewski.nikodem@gmail.com>
2023-07-16 14:05:07 +0200

    changed database.ts files

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -4,11 +4,11 @@ export default ({ env }) => ({
     connection: {
       host: env(
         "DATABASE_HOST",
-        "db-strapi.cxcmwaj0diye.eu-north-1.rds.amazonaws.com"
+        "strapi-database.cxcmwaj0diye.eu-north-1.rds.amazonaws.com"
       ),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "db-strapi"),
-      user: env("DATABASE_USERNAME", "postgres"),
+      database: env("DATABASE_NAME", "strapi-database"),
+      user: env("DATABASE_USERNAME", "strapidb_user"),
       password: env("DATABASE_PASSWORD", "3p7-gQSA?w-"),
     },
     useNullAsDefault: true,
